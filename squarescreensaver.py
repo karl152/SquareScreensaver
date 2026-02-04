6ec19e536d65bc2d923ba72b27ed9564f5ec09b9
Karl <karl.david.wes@icloud.com>
2026-02-04 01:16:12 +0100

    add settings menu with many settings
    This may be the largest commit in the project history, as I added nearly everything that I could think of.
    Sure, there could be some other stuff, but the most important things are here, including correct DPI scaling.

--- squarescreensaver.py
+++ squarescreensaver.py
@@ -7,9 +7,71 @@
 from turtle import *
 import random
 import sys
+import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import colorchooser
+import ctypes
+import winreg
+import subprocess
 
-ver = "1.0"
+ver = "1.1"
+failcounter = 0
+
+def loadRegistry():
+    global failcounter, BackgroundImagePath, CloseOnMouseMovement, Thickness, Speed, UseBackgroundImage, Squares, R, G, B
+    if failcounter >= 2:
+        messagebox.showerror("Error", "Loop while trying to create registry")
+        sys.exit(1)
+    try:
+        SQSKey = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\SquareScreensaver\\")
+        ConfigVersion = winreg.QueryValueEx(SQSKey, "ConfigVersion")[0]
+        BackgroundImagePath = winreg.QueryValueEx(SQSKey, "BackgroundImagePath")[0]
+        CloseOnMouseMovement = winreg.QueryValueEx(SQSKey, "CloseOnMouseMovement")[0]
+        Thickness = winreg.QueryValueEx(SQSKey, "Thickness")[0]
+        Speed = winreg.QueryValueEx(SQSKey, "Speed")[0]
+        UseBackgroundImage = winreg.QueryValueEx(SQSKey, "UseBackgroundImage")[0]
+        Squares = winreg.QueryValueEx(SQSKey, "Squares")[0]
+        R = winreg.QueryValueEx(SQSKey, "BackgroundR")[0]
+        G = winreg.QueryValueEx(SQSKey, "BackgroundG")[0]
+        B = winreg.QueryValueEx(SQSKey, "BackgroundB")[0]
+        # print(f"ConfigVersion: {ConfigVersion}\nBackgroundImagePath: {BackgroundImagePath}\nCloseOnMouseMovement: {CloseOnMouseMovement}\nThickness: {Thickness}\nSpeed: {Speed}\nUseBackgroundImage: {UseBackgroundImage}\nSquares: {Squares}\nbackground colors: {R} {G} {B}")
+        winreg.CloseKey(SQSKey)
+    except:
+        failcounter += 1
+        createRegistry()
+
+def rgb_to_hex(r, g, b):
+    return f"#{r:02x}{g:02x}{b:02x}"
+
+def clearRegistry(exitafter):
+    keypath = r"Software\\SquareScreensaver\\"
+    try:
+        whatever = subprocess.getoutput(r'reg delete "HKEY_CURRENT_USER\Software\SquareScreensaver" /f') # sorry, but I'm not figuring out how to do that with winreg right now
+        print(whatever)
+    except:
+        pass
+    if exitafter == True:
+        sys.exit(0)
+def createRegistry():
+    clearRegistry(False)
+    SoftwareKey = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\")
+    SQSKey = winreg.CreateKey(SoftwareKey, "SquareScreensaver")
+    # default settings
+    winreg.SetValueEx(SQSKey, "ConfigVersion", 0, winreg.REG_DWORD, 1)
+    winreg.SetValueEx(SQSKey, "BackgroundImagePath", 0, winreg.REG_SZ, "")
+    winreg.SetValueEx(SQSKey, "CloseOnMouseMovement", 0, winreg.REG_DWORD, 1)
+    winreg.SetValueEx(SQSKey, "Thickness", 0, winreg.REG_DWORD, 1)
+    winreg.SetValueEx(SQSKey, "Speed", 0, winreg.REG_DWORD, 0)
+    winreg.SetValueEx(SQSKey, "UseBackgroundImage", 0, winreg.REG_DWORD, 0)
+    winreg.SetValueEx(SQSKey, "Squares", 0, winreg.REG_DWORD, 1000)
+    winreg.SetValueEx(SQSKey, "BackgroundR", 0, winreg.REG_DWORD, 0)
+    winreg.SetValueEx(SQSKey, "BackgroundG", 0, winreg.REG_DWORD, 0)
+    winreg.SetValueEx(SQSKey, "BackgroundB", 0, winreg.REG_DWORD, 0)
+    winreg.CloseKey(SQSKey)
+    loadRegistry()
+
 if "/p" in sys.argv:
     sys.exit(0)
 elif "/?" in sys.argv:
@@ -18,25 +80,37 @@ elif "/?" in sys.argv:
 /c open options menu
 /? show this""")
     sys.exit(0)
-elif "/s" in sys.argv: 
+elif "/s" in sys.argv:
+    loadRegistry()
     def exit_input(event=None):
         screen.bye()
         sys.exit(0)
 
     try:
         colormode(255)
-        speed(0)
+        speed(Speed)
         screen = Screen()
+        try:
+            ctypes.windll.shcore.SetProcessDpiAwareness(1)
+        except:
+            pass
         screen.cv._rootwindow.attributes("-fullscreen", True)
-        screen.cv._rootwindow.bind("<Motion>", exit_input)
+        if CloseOnMouseMovement == 1:
+            screen.cv._rootwindow.bind("<Motion>", exit_input)
         screen.cv._rootwindow.bind("<Key>", exit_input)
         screen.cv._rootwindow.config(cursor="none")
         height = screen.cv._rootwindow.winfo_screenheight()
         width = screen.cv._rootwindow.winfo_screenwidth()
         halfwidth = int(width/2)
         halfheight = int(height/2)
-        screen.bgcolor(0, 0, 0)
-        for i in range (1000):
+        screen.bgcolor(R, G, B)
+        if UseBackgroundImage == 1:
+            try:
+                screen.bgpic(BackgroundImagePath)
+            except:
+                pass
+        pensize(Thickness)
+        for i in range (Squares):
             x=random.randint(int(f"-{halfwidth}"), halfwidth)
             y=random.randint(int(f"-{halfheight}"), halfheight)
             penup()
@@ -56,5 +130,84 @@ elif "/s" in sys.argv:
     except:
         sys.exit(0)
 else:
-    messagebox.showinfo(f"SquareScreensaver {ver}", "no options available")
-    sys.exit(0)
+    loadRegistry()
+    def ChangeColor():
+        global R, G, B
+        ChosenColor = tk.colorchooser.askcolor()
+        R = ChosenColor[0][0]
+        G = ChosenColor[0][1]
+        B = ChosenColor[0][2]
+        BackgroundColorThing.config(bg=ChosenColor[1])
+    def openBackgroundImage():
+        BackgroundPicturePath = filedialog.askopenfilename(title="Choose Background Picture", filetypes=[("Pictures", "*.png *.gif *.pgm *.ppm")])
+        BackgroundPathEntry.delete(0, tk.END)
+        BackgroundPathEntry.insert(0, BackgroundPicturePath)
+    def clearConfig():
+        clearRegistry(False)
+        Window.destroy()
+        sys.exit(0)
+    def saveandclose():
+        global BackgroundImagePath, CloseOnMouseMovement, Thickness, Speed, UseBackgroundImage, Squares, R, G, B
+        try:
+            SQSKey = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, r"Software\\SquareScreensaver\\", 0, winreg.KEY_SET_VALUE)
+            winreg.SetValueEx(SQSKey, "BackgroundImagePath", 0, winreg.REG_SZ, BackgroundPathEntryTk.get())
+            winreg.SetValueEx(SQSKey, "CloseOnMouseMovement", 0, winreg.REG_DWORD, CloseScreensaverOnMouseMovementBooleanTk.get())
+            winreg.SetValueEx(SQSKey, "Thickness", 0, winreg.REG_DWORD, ThicknessTk.get())
+            winreg.SetValueEx(SQSKey, "Speed", 0, winreg.REG_DWORD, SpeedTk.get())
+            winreg.SetValueEx(SQSKey, "UseBackgroundImage", 0, winreg.REG_DWORD, UseBackgroundImageTk.get())
+            winreg.SetValueEx(SQSKey, "Squares", 0, winreg.REG_DWORD, SquaresTk.get())
+            winreg.SetValueEx(SQSKey, "BackgroundR", 0, winreg.REG_DWORD, R)
+            winreg.SetValueEx(SQSKey, "BackgroundG", 0, winreg.REG_DWORD, G)
+            winreg.SetValueEx(SQSKey, "BackgroundB", 0, winreg.REG_DWORD, B)
+            winreg.CloseKey(SQSKey)
+        except:
+            messagebox.showerror("Error", "Error while trying to save settings")
+        Window.destroy()
+    Window = tk.Tk()
+    Window.resizable(False, False)
+    SpeedTk = tk.IntVar(value=Speed)
+    ThicknessTk = tk.IntVar(value=Thickness)
+    UseBackgroundImageTk = tk.IntVar(value=UseBackgroundImage)
+    SquaresTk = tk.IntVar(value=Squares)
+    BackgroundPathEntryTk = tk.StringVar(value=BackgroundImagePath)
+    CloseScreensaverOnMouseMovementBooleanTk = tk.IntVar(value=CloseOnMouseMovement) # this was a boolean, but it's now an integer because that works easier with the registry
+    Window.title("Settings")
+    BigText = tk.Label(Window, text="SquareScreensaver", font=("Lucida Console", 15))
+    SmallText = ttk.Label(Window, text=f"Version {ver}")
+    SpeedBox = ttk.LabelFrame(Window, text="Speed")
+    SpeedSlider = tk.Scale(SpeedBox, from_=0, to=5, orient="horizontal", variable=SpeedTk)
+    ThicknessBox = ttk.LabelFrame(Window, text="Thickness") # or should I call it pen size?
+    ThicknessSlider = tk.Scale(ThicknessBox, from_=0, to=5, orient="horizontal", variable=ThicknessTk)
+    BackgroundBox = ttk.LabelFrame(Window, text="Background")
+    BackgroundColorThing = tk.Label(BackgroundBox, text="_____", bg=rgb_to_hex(R, G, B))
+    BackgroundColorButton = ttk.Button(BackgroundBox, text="Change Color", command=ChangeColor)
+    BackgroundImageCheckbox = ttk.Checkbutton(BackgroundBox, text="use background image", variable=UseBackgroundImageTk)
+    BackgroundOpenButton = tk.Button(BackgroundBox, text="1", font=("Wingdings", 10), command=openBackgroundImage)
+    BackgroundPathEntry = ttk.Entry(BackgroundBox, textvariable=BackgroundPathEntryTk)
+    OtherThingsBox = ttk.LabelFrame(Window, text="Other things")
+    SquaresLabel = ttk.Label(OtherThingsBox, text="Squares: ")
+    SquaresSpinbox = ttk.Spinbox(OtherThingsBox, width=5, from_=0, to=99999, textvariable=SquaresTk)
+    CloseOnMouseMovementCheckbox = ttk.Checkbutton(OtherThingsBox, text="stop on mouse movement", variable=CloseScreensaverOnMouseMovementBooleanTk)
+    ClearConfigButton = ttk.Button(OtherThingsBox, text="Clear Configuration", command=clearConfig)
+    BigText.grid(row=0, column=0, columnspan=2)
+    SmallText.grid(row=1, column=0, columnspan=2)
+    ttk.Separator(Window, orient="horizontal").grid(row=2, column=0, columnspan=2, sticky="news")
+    SpeedBox.grid(row=3, column=0, pady=5)
+    SpeedSlider.grid(row=0, column=0)
+    ThicknessBox.grid(row=3, column=1, pady=5)
+    ThicknessSlider.grid(row=0, column=0)
+    BackgroundBox.grid(row=4, column=0, columnspan=2, padx=10, pady=5)
+    BackgroundColorThing.grid(row=0, column=0)
+    BackgroundColorButton.grid(row=0, column=1)
+    BackgroundImageCheckbox.grid(row=0, column=2)
+    BackgroundOpenButton.grid(row=1, column=0)
+    BackgroundPathEntry.grid(row=1, column=1, columnspan=2, sticky="news")
+    OtherThingsBox.grid(row=5, column=0, columnspan=2, pady=5)
+    SquaresLabel.grid(row=0, column=0)
+    SquaresSpinbox.grid(row=0, column=1)
+    CloseOnMouseMovementCheckbox.grid(row=0, column=2)
+    ClearConfigButton.grid(row=1, columnspan=3, pady=5)
+    ttk.Separator(Window, orient="horizontal").grid(row=6, column=0, columnspan=2, sticky="news")
+    ttk.Button(Window, text="OK", command=saveandclose).grid(row=7, column=0)
+    ttk.Button(Window, text="Cancel", command=lambda: Window.destroy()).grid(row=7, column=1)
+    Window.mainloop()
